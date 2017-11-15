ee348fac6ebee8a63ad92e617786626d8b2be338
Razvan Serban <razvan_tankian@yahoo.com>
2017-11-15 22:42:11 +0200

    Added the main menu

---
+++ Assets/Scripts/GameData/ButtonManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour {
+
+    public void NewGameBtn(string newGameLevel)
+    {
+        SceneManager.LoadSceneAsync(newGameLevel);
+    }
+
+    public void QuitGameBtn()
+    {
+        Application.Quit();
+    }
+
+    public void SettingsBtn(Transform menuTransform)
+    {
+       
+        Camera.main.transform.LookAt(menuTransform.position);
+    }
+}
